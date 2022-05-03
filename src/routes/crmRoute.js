a08efd41af865eaa68bc6ad8ba53ca9acf766900
Hugo Lara <hugo.alex200197@outlook.com>
2022-05-03 17:30:22 -0500

    Added middleware

--- src/routes/crmRoute.js
+++ src/routes/crmRoute.js
@@ -1,7 +1,10 @@
 const routes = (app) => {
   app.route('/contact')
     .get((req,res,next) => {
-      
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, (req, res, next) => {
       res.send('GET request successful');
     })
 
