f39ec1aadb2876a56914cd440a8088aa912c9bce
Hugo Lara <hugo.alex200197@outlook.com>
2022-05-03 17:23:36 -0500

    Added endpoint in route documen

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const routes = require('./routes/crmRoute');
 
 const app = express();
 const PORT = 3000;
 
+routes(app);
+
 app.get('/', (req, res) => {
   res.send(`Node and express running on port ${PORT}`);
 });
-
-app.get('/api', (req, res) => {
-  res.send(`Endpoint api`);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
---
+++ src/routes/crmRoute.js
@@ -0,0 +1,26 @@
+const routes = (app) => {
+  app.route('/contact')
+    .get((req,res,next) => {
+      
+      res.send('GET request successful');
+    })
+
+    .post((req, res) => {
+      res.send('POST request was successful');
+    });
+  
+  app.route('/contact/:contactId')
+    .get((req,res) => {
+      let contactId = req.params.contactId;
+      res.send('GET ${contactId} request successful');
+    })
+    .put((req, res) => {
+      res.send('PUT request successful');
+    })
+    .delete((req, res) => {
+      res.send('DELETE request successful');
+    });
+
+};
+
+module.exports = routes;
