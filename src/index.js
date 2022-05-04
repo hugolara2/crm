b8cf58aa761893c181443b34c040baf8830d9f2c
Hugo Lara <hugo.alex200197@outlook.com>
2022-05-04 11:11:23 -0500

    Added conection to the database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes/crmRoute');
+const uri = "mongodb://localhost/CRM";
+
+//Mongoose conection
+mongoose.Promise = global.Promise;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
+
+//body parser
+app.use(bodyParser.urlencode({ extended: true }));
+app.use(bodyParser.json());
+
 const PORT = 3000;
 
 routes(app);
